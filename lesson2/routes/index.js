3e297439a12565bea7705d688924885183b3d9da
wulichenyang <943934049@qq.com>
2019-06-23 23:22:56 +0800

    Add cookie and async

--- lesson2/routes/index.js
+++ lesson2/routes/index.js
@@ -50,6 +50,7 @@ router.get('/topic', function (req, res, next) {
 });
 
 // 异步爬虫
+// 异步请求数控制在10以内的情况
 router.get('/comment', function (req, res, next) {
   superagent.get(cnodeUrl)
     .end((err, sres) => {
@@ -75,7 +76,7 @@ router.get('/comment', function (req, res, next) {
           return ({
             title: $('.topic_full_title').text().trim(),
             href: topicPair[0],
-            comment0: $('.reply_content').eq(0).text().trim(), 
+            comment0: $('.reply_content').eq(0).text().trim(),
           })
 
         })
@@ -99,43 +100,67 @@ router.get('/comment', function (req, res, next) {
 })
 
 let fetchUrl = function (url, callback) {
-  var delay = parseInt((Math.random() * 10000000) % 2000, 10);
-  concurrencyCount++;
-  console.log('现在的并发数是', concurrencyCount, '，正在抓取的是', url, '，耗时' + delay + '毫秒');
-  setTimeout(function () {
-    concurrencyCount--;
-    callback(null, url + ' html content');
-  }, delay);
+  // var delay = parseInt((Math.random() * 10000000) % 2000, 10);
+  // concurrencyCount++;
+  // console.log('现在的并发数是', concurrencyCount, '，正在抓取的是', url, '，耗时' + delay + '毫秒');
+  // setTimeout(function () {
+  //   concurrencyCount--;
+  //   callback(null, url + ' html content');
+  // }, delay);
+  superagent.get(url)
+    .end((err, res) => {
+      callback(null, [url, res.text])
+      // let $ = cheerio.load(res.text())
+      // $('.topic_full_title').text().trim(), $('.reply_content').eq(0).text().trim()]
+    })
 };
 
 // 异步控制并发数爬虫
-// router.get('/async', function (req, res, next) {
-//   superagent.get(cnodeUrl)
-//     .end((err, sres) => {
-//       if (err) {
-//         next(err)
-//       }
-//       let topicUrls = []
-//       let $ = cheerio.load(sres.text)
-//       $('#topic_list .topic_title').each((i, e) => {
-//         let $e = $(e)
-//         let href = url.resolve(cnodeUrl, $e.attr('href'))
-//         topicUrls.push(href)
-//       })
+// 异步请求的数量超出10
+router.get('/async', function (req, res, next) {
+  superagent.get(cnodeUrl)
+    .end((err, sres) => {
+      if (err) {
+        next(err)
+      }
+      let topicUrls = []
+      let $ = cheerio.load(sres.text)
+      $('#topic_list .topic_title').each((i, e) => {
+        let $e = $(e)
+        let href = url.resolve(cnodeUrl, $e.attr('href'))
+        topicUrls.push(href)
+      })
 
-//       // 得到 topicUrls 之后
-//       async.mapLimit(topicUrls, 5, function (url, callback) {
-//         fetchUrl(url, callback);
-//       }, function (err, result) {
-//         res.send({
-//           status: 0,
-//           data: {
-//             result
-//           },
-//           message: 'ok',
-//         })
-//       });
-//     })
-// })
+      // 得到 topicUrls 之后
+      async.mapLimit(topicUrls, 8, function (url, callback) {
+        fetchUrl(url, callback);
+      }, function (err, results) {
+        results = results.map((pair, i) => {
+          let $ = cheerio.load(pair[1])
+          return {
+            title: $('.topic_full_title').text().trim(),
+            href: pair[0],
+            comment0: $('.reply_content').eq(0).text().trim()
+          }
+        })
+        res.send({
+          status: 0,
+          data: results,
+          message: 'ok',
+        })
+      });
+    })
+})
+
+// Set cookie
+router.get('/cookie', function (req, res, next) {
+  if(req.cookies.isVisited) {
+    console.log(req.cookies)
+    res.send('很高兴再次见到你！')
+  } else {
+    res.cookie('isVisited', true, {maxAge: 60 * 1000}) // Expire in one minute
+    res.send('初次见面，你好！')
+  }
+})
 
 module.exports = router;
